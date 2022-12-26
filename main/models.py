811da896a04a789baaec5d67be489384e0508124
ahmad.sahmarany1234 <ahmad.sahmarany1234@gmail.com>
2022-12-27 00:07:56 +0300

    first commit

--- main/models.py
+++ main/models.py
@@ -6,4 +6,13 @@ class Category(models.Model):
     
     def __str__(self):
         return self.title
-     
+    
+class News(models.Model):
+    Category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    image=models.ImageField(upload_to='imgs/')
+    detail=models.TextField()
+    add_time=models.DateTimeField(auto_now_add=True)
+        
+    def __str__(self):
+        return self.title
